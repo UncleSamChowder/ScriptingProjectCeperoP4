5015b8bb90f17eb73937d523408b4c0542ed75c2
UncleSamChowder <745603@student.dusd.net>
2024-10-03 11:09:24 -0700

    2nd scripting assignment
    Added a scripting using my int

--- ScriptingProjectP4Cepero/Assets/Scripts/ColorChanger.cs
+++ ScriptingProjectP4Cepero/Assets/Scripts/ColorChanger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
@@ -17,7 +18,7 @@ public class ColorChanger : MonoBehaviour
         {
             GetComponent<Renderer>().material.color = Color.red;
         }
-
+      
         if (Input.GetKeyDown(KeyCode.G))
         {
             GetComponent<Renderer>().material.color = Color.green;
@@ -31,6 +32,36 @@ public class ColorChanger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             GetComponent<Renderer>().material.color = Color.cyan;   
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Renderer>().material.color= Color.magenta;
+        }
+
+        if( Input.GetKeyDown(KeyCode.S))
+        {
+            GetComponent<Renderer>().material.color=Color.white;
+        }
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color=Color.black;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Renderer>().material.color=Color.yellow;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            GetComponent<Renderer>().material.color=Color.grey;
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color=Color.clear;
         }
     }
 }
